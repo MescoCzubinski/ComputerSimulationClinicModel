c8bb8cbbbf06408be0a00616eac4436887c0ff1e
MescoCzubinski <mieszkoczubinski@gmail.com>
2025-11-19 21:27:03 +0100

    distributions update

--- distributions.py
+++ distributions.py
@@ -6,24 +6,25 @@ average_appointment_duration = 17
 appointment_reserved_time = 20
 
 clinic_open_time = 0
-clinic_close_time = 800
+clinic_close_time = 600 # 10 hours in minutes
 
 scheduled_registration_time_multiplier = 1.0
 run_walk_in_registration_time_multiplier = 1.5 # walk-in patients take longer at registration
 
 def get_walk_in_arrival_time_dist(num_patients):
-    nine_o_clock = 100
-    two_o_clock = 600
-    dist_nine_o_clock = np.random.normal(loc=nine_o_clock, scale=120, size=num_patients)
-    dist_two_o_clock = np.random.normal(loc=two_o_clock, scale=120, size=num_patients)
-    combined_dist = np.concatenate([dist_nine_o_clock, dist_two_o_clock])
+    first_pic = 120
+    second_pic = 420
+    dist_first_pic = np.random.normal(loc=first_pic, scale=120, size=num_patients)
+    dist_second_pic = np.random.normal(loc=second_pic, scale=120, size=num_patients)
+    combined_dist = np.concatenate([dist_first_pic, dist_second_pic])
     return combined_dist[(combined_dist >= clinic_open_time) & (combined_dist <= clinic_close_time)]
 
-# TODO: we can simulate real scheduled appoitnemt time (not just every 20 minutes but with some human-planned distribution)
 def get_scheduled_arrival_time_dist(num_patients, num_doctors):
+    avg_space_time = max(clinic_close_time // (num_patients // num_doctors), appointment_reserved_time)
+
     appointment_times = []
     for i in range(num_patients):
-        appointment_times.append((i // num_doctors) * appointment_reserved_time)
+        appointment_times.append((i // num_doctors) * avg_space_time)
     return appointment_times
 
 def get_registration_duration_dist(num_patients, multiplier):
@@ -31,11 +32,6 @@ def get_registration_duration_dist(num_patients, multiplier):
 
 def get_appointment_duration_dist(num_patients):
     return np.random.gamma(shape=average_appointment_duration, scale=1, size=num_patients) + 1 # minimum 1 minute
-
-def time_to_minutes(val):
-    hours = int(val) // 100
-    minutes = int(val) % 100
-    return (hours) * 60 + minutes
 
 def get_scheduled_patients(num_patients, num_doctors):
     patients = [
@@ -52,7 +48,7 @@ def get_scheduled_patients(num_patients, num_doctors):
 def get_run_walk_in_patients(num_patients):
     patients = [
         (
-            int(time_to_minutes(np.random.choice(get_walk_in_arrival_time_dist(num_patients)))),
+            int(np.random.choice(get_walk_in_arrival_time_dist(num_patients))),
             int(np.random.choice(get_registration_duration_dist(num_patients, run_walk_in_registration_time_multiplier))),
             int(np.random.choice(get_appointment_duration_dist(num_patients)))
         )
@@ -63,11 +59,22 @@ def get_run_walk_in_patients(num_patients):
     
     return patients
 
-# plt.hist(get_appointment_duration_dist(100), bins=50, density=True)
+# max patients per doctor
+# print(get_scheduled_patients(90, 3))
+# print(get_run_walk_in_patients(90))
+
+# avg pacients per doctor
+# print(get_scheduled_patients(50, 3))
+# print(get_run_walk_in_patients(50))
+
+# appointment duration distribution plot
+# plt.hist(get_appointment_duration_dist(1000), bins=50, density=True)
 # plt.show()
 
-# plt.hist(get_walk_in_arrival_time_dist(100), bins=50, density=True)
+# walk-in arrival time distribution plot
+# plt.hist(get_walk_in_arrival_time_dist(1000), bins=50, density=True)
 # plt.show()
 
-# plt.hist(get_registration_duration_dist(100, 1), bins=50, density=True)
+# registration duration distribution plot
+# plt.hist(get_registration_duration_dist(1000, 1), bins=50, density=True)
 # plt.show()
