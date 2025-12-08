f24576c18d08e0c841dc671bc9cb5f16c334ee5f
JGTB0PL <jgtb0pl@gmail.com>
2025-12-08 19:33:20 +0100

    wait time printing fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import simulation as sim
 
+def pprint(lst):
+    print(list(map(float, lst)))
+
 def main():
-    scheduled_result = sim.run_scheduled_simulation()
+    scheduled_result = sim.run_scheduled_simulation(3, 10, 300)
     walk_in_result = sim.run_walk_in_simulation(3, 10, 300)
 
     print("\nZ umawianym terminem:")
@@ -10,7 +13,7 @@ def main():
     print("Bez umawiania terminu:")
     print(walk_in_result.idle_times, walk_in_result.avg_time)
 
-    print(walk_in_result.patient_times_wait)
+    pprint(walk_in_result.patient_times_wait)
 
 if __name__ == "__main__":
     main()
